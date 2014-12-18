3b53be5a03e7c93a5ffdfdb90448b498b3c1abea
vtphan <vphan@memphis.edu>
2014-12-18 14:47:08 -0600

    replace print with cat

--- scripts/cq.R
+++ scripts/cq.R
@@ -13,7 +13,7 @@ args<-commandArgs(TRUE)
 # args[4] - z (optional)
 
 if (length(args) < 3 || length(args) > 4) {
-   print("Usage:  Rscript qq.R data_file x y [z]")
+   cat("Usage:  Rscript qq.R data_file x y [z]\n")
 } else {
    sep <- "\t"
    if (file_ext(args[1]) == "csv") {
@@ -28,20 +28,20 @@ if (length(args) < 3 || length(args) > 4) {
       }
       p
       ggsave("output1.png")
-      print("Barplot saved to output1.png")
+      cat("Barplot saved to output1.png\n")
       p <- ggplot(data, aes(x=factor(eval(parse(text=args[2]))), y=eval(parse(text=args[3])))) + geom_boxplot() + xlab(args[2]) + ylab(args[3])
       if (length(args)==4) {
          p <- p + facet_wrap(eval(parse(text=paste("~",args[4]))))
       }
       p
       ggsave("output2.png")
-      print("Boxplot saved to output2.png")
+      cat("Boxplot saved to output2.png\n")
       p <- ggplot(data, aes(x=factor(eval(parse(text=args[2]))), y=eval(parse(text=args[3])))) + geom_violin(trim=FALSE) +  stat_summary(fun.y=data_summary, fun.ymin=min, fun.ymax=max) + xlab(args[2]) + ylab(args[3])
       if (length(args)==4) {
          p <- p + facet_wrap(eval(parse(text=paste("~",args[4]))))
       }
       p
       ggsave("output3.png")
-      print("Violinplot saved to output3.png")
+      cat("Violinplot saved to output3.png\n")
    }
 }
--- scripts/qq.R
+++ scripts/qq.R
@@ -9,7 +9,7 @@ args<-commandArgs(TRUE)
 # args[4] - z (optional)
 
 if (length(args) < 3 || length(args) > 4) {
-   print("Usage:  Rscript qq.R data_file x y [z]")
+   cat("Usage:  Rscript qq.R data_file x y [z]\n")
 } else {
    sep <- "\t"
    if (file_ext(args[1]) == "csv") {
@@ -20,13 +20,13 @@ if (length(args) < 3 || length(args) > 4) {
    if (length(args) == 3) {
       ggplot(data, aes(x=eval(parse(text=args[2])), y=eval(parse(text=args[3])))) + geom_point() + xlab(args[2]) + ylab(args[3])
       ggsave("output.png")
-      print("Output saved to output.png")
+      cat("Output saved to output.png\n")
    } else {
       ggplot(data, aes(x=eval(parse(text=args[2])), y=eval(parse(text=args[3])), color=eval(parse(text=args[4])))) + geom_point() + xlab(args[2]) + ylab(args[3]) + labs(color=args[4])
       ggsave("output1.png")
-      print("Output saved to output1.png")
+      cat("Output saved to output1.png\n")
       ggplot(data, aes(x=eval(parse(text=args[2])), y=eval(parse(text=args[3])))) + geom_point() + facet_wrap(eval(parse(text=paste("~",args[4])))) + xlab(args[2]) + ylab(args[3])
       ggsave("output2.png")
-      print("Output saved to output2.png")
+      cat("Output saved to output2.png\n")
    }
 }
